0976edfb3d542a1a26d1f1e20a6ee65ea39f8041
icuthy <icuthy@gmail.com>
2019-01-29 16:42:40 -0800

    [^] Add OracleVideosOnly toggle button and show video results only from oracle dev channel

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import youtube from '../apis/youtube';
 
 class App extends React.Component {
 
-    state = { videos: [], selectedVideo: null }
+    state = { videos: [], selectedVideo: null, channelId: null, results: 0 }
 
     // to avoid blank video area, create default video when App component first rendered to screen
     componentDidMount() {
@@ -17,12 +17,14 @@ class App extends React.Component {
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
-                q: term
+                q: term,
+                channelId: this.state.channelId
             }
         });
         this.setState({
             videos: response.data.items,
-            selectedVideo: response.data.items[0] //default to showing first video if none selected from list
+            selectedVideo: response.data.items[0], //default to showing first video if none selected from list
+            results: response.data.pageInfo.totalResults,
         });
     };
 
@@ -30,24 +32,55 @@ class App extends React.Component {
         this.setState({ selectedVideo: video });
     };
 
+    onCheckOracleVideos = async (event) => {
+        event.target.checked === true ? this.setState({ channelId: 'UCdDhYMT2USoLdh4SZIsu_1g' }) : this.setState({ channelId: null })
+    };
+
     render() {
-        return (
-            <Fragment>
-                <div className="ui container">
-                    <SearchBar onFormSubmit={this.onTermSubmit} />
-                    <div className="ui grid">
-                        <div className="ui row">
-                            <div className="eleven wide column">
-                                <VideoDetail video={this.state.selectedVideo} />
-                            </div>
-                            <div className="five wide column">
-                                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+
+        if (this.state.results === 0) {
+            return (
+                <Fragment>
+                    <div className="ui container paddingtop">
+                        <SearchBar onFormSubmit={this.onTermSubmit} />
+                        <div className="ui toggle checkbox">
+                            <input type="checkbox" name="oraclevideos" value="true" onChange={(event) => this.onCheckOracleVideos(event)} />
+                            <label htmlFor="oraclevideos">Oracle Only</label>
+                            <br />
+                        </div>
+                        <div className="ui grid">
+                            <div className="ui row">
+                                <div className="sixteen wide column">
+                                    Your search provided no results, please try a different search term.
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </Fragment>
-        )
+                </Fragment>
+            )
+        } else
+            return (
+                <Fragment>
+                    <div className="ui container paddingtop">
+                        <SearchBar onFormSubmit={this.onTermSubmit} />
+                        <div className="ui toggle checkbox">
+                            <input type="checkbox" name="oraclevideos" value="true" onChange={(event) => this.onCheckOracleVideos(event)} />
+                            <label htmlFor="oraclevideos">Oracle Only</label>
+                            <br />
+                        </div>
+                        <div className="ui two column stackable grid">
+                            <div className="ui row">
+                                <div className="eleven wide column">
+                                    <VideoDetail video={this.state.selectedVideo} />
+                                </div>
+                                <div className="five wide column">
+                                    <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Fragment>
+            )
     }
 }
 
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
-    const renderedList = videos.map((video) => {
-        return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />
+    const renderedList = videos.map((video, key) => {
+        return <VideoItem key={key} video={video} onVideoSelect={onVideoSelect} />
     })
     return (
         <div className="ui relaxed divided list">
